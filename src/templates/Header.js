40e1dfcecfb8fc1befcfe193b1d16eb1a9954b2f
aguscorvo <aguscorvo@gmail.com>
2021-02-26 20:59:03 -0300

    Fix header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">100tifi.co</a>
+          <a href="/100tifi.co">100tifi.co</a>
         </h1>
       </div>
       <div class="Header-nav">
