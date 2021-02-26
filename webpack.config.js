cc14d8fa7121cc74bd8d0f66021908c7599773c8
aguscorvo <aguscorvo@gmail.com>
2021-02-26 20:43:46 -0300

    CSS and travis config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -26,6 +27,9 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/styles/styles.css', to: '' }],
     }),
   ],
 };
